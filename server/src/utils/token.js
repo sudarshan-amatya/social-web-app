61bba0445235be3d1fa8e729e953d6d89cedae57
Sudarshan Amatya <sudarshanamatya1122@gmail.com>
2026-04-04 12:44:42 +0545

    Remove zip files and update project

--- server/src/utils/token.js
+++ server/src/utils/token.js
@@ -1,3 +1,11 @@
+import jwt from "jsonwebtoken"
+
+export function generateToken(userId) {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  })
+}
+
 export function setAuthCookie(res, token) {
   const isProduction = process.env.NODE_ENV === "production"
 
