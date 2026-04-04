b8a3b0ebc99d5268e5de8d00cf5a5c6af1007ca4
Sudarshan Amatya <sudarshanamatya1122@gmail.com>
2026-04-04 11:50:59 +0545

    s

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://social-web-app-3.onrender.com",
+  baseURL: "https://social-web-app-3.onrender.com/api",
   withCredentials: true,
 })
